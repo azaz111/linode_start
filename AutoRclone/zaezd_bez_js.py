2fce33cedfe5ecd71129ac995ebdbb69284a9f55
azaz111 <86608104+azaz111@users.noreply.github.com>
2022-08-23 10:23:09 +0300

    Update zaezd_bez_js.py

--- AutoRclone/zaezd_bez_js.py
+++ AutoRclone/zaezd_bez_js.py
@@ -8,55 +8,52 @@ import os , math , time
 import json
 import subprocess
 import threading
-from BIB_API import service_avtoriz , perenos_fails_list , spisok_fails_q ,new_drive , \
-           spisok_fails_roditelya,createRemoteFolder,peremesti_v_odnu , drive_ls ,_is_success , folder_all
+from BIB_API import service_avtoriz , drive_ls , folder_all
+
 def perenos(pap,drive):
+    service_mp = service_avtoriz()
     try:
-        file = service.files().get(fileId=pap, supportsAllDrives=True, fields='parents').execute()
+        file = service_mp.files().get(fileId=pap, supportsAllDrives=True, fields='parents').execute()
         previous_parents = ",".join(file.get('parents'))
 
         print('Перекидываю ')
-        file = service.files().update(fileId=pap,
+        file = service_mp.files().update(fileId=pap,
                                       addParents=drive,
                                       supportsAllDrives=True, 
                                       removeParents=previous_parents, fields='id, parents').execute()
+        return
     except HttpError as err: 
         print('ОШИБКА : ' + err)
         if err.resp.get('content-type', '').startswith('application/json'):
             reason = json.loads(err.content).get('error').get('errors')[0].get('reason')
             print('ОШИБКА : ' + reason)
             print('Ждем дополнительные 10 секунд')
-            time.sleep(100) 
-successful = []
+            time.sleep(10) 
+        perenos(pap,drive)
+    return
 
 service = service_avtoriz()
-sp_paps=folder_all(service)
-sp_paps_name=[eee['name'] for eee in sp_paps ]
-try:
-    sp_paps_name.remove('PLOT')
-except:
+paps_perens=folder_all(service)
+sp_folders=[iii for iii in paps_perens if iii['name'] != 'PLOT']
+plot_folders=[iii['parents'][0] for iii in paps_perens if iii['name'] == 'PLOT'][0]
+if plot_folders:
+    print('Найшли Plot', plot_folders)
+
     pass
-sp_paps_name=[eee[:-4] for eee in sp_paps_name ]
-got_grive=[]
-drive_bibl={}
-pap_bibl={}
-for ddd in drive_ls(service):
-    if ddd['name'] in sp_paps_name:
-        drive_bibl[ddd['name']]=ddd['id']
-for ddd in sp_paps:
-    if ddd['name'][:-4] in sp_paps_name:
-        pap_bibl[ddd['name'][:-4]]=ddd['id']
+else:
+    input('Ненайдена папка PLOT')
+drive_lsf=drive_ls(service)
+drive_lsf=[iii['id'] for iii in drive_lsf if iii['id'] != plot_folders]
 
-print(list(pap_bibl))
-print(len(list(pap_bibl)))
+if len(drive_lsf) < len(sp_folders):
+    input('Немогу разложить нехватает дисков')
 
-for i in list(pap_bibl):
-
-    print('Hа диск :'+ i +' Id : '+ drive_bibl[i])
-    print('папка ', pap_bibl[i])
-    #perenos(pap_bibl[i],drive_bibl[i])
-    thread = threading.Thread(target=perenos, args=(pap_bibl[i],drive_bibl[i],))
+for i , folders in enumerate(sp_folders):
+    print('Папка  :'+ folders['id'] +' Id drive : '+ drive_lsf[i])
+    time.sleep(0.2)
+    thread = threading.Thread(target=perenos, args=(folders['id'],drive_lsf[i],))
     thread.start()
+    #perenos(folders['id'],drive_lsf[i])
 
 print('ГОТОВО')
      
